8107d1e0ec06d63dbec94d38e37c4f16caec0160
Muhammad Anas Qadri <161422002+Anas-Rajput12@users.noreply.github.com>
2026-01-02 17:15:35 +0500

    Update RagChatbot.jsx

--- src/components/RagChatbot.jsx
+++ src/components/RagChatbot.jsx
@@ -3,7 +3,7 @@ import { FaComments, FaTrash } from "react-icons/fa";
 import "./Chatbot.css";
 
 const BACKEND_URL =
-  import.meta.env.REACT_APP_BACKEND_URL || "https://backend-12-teal.vercel.app/";
+  import.meta.env.REACT_APP_BACKEND_URL || "localhost:8000";
 
 export default function RagChatbot() {
   const [open, setOpen] = useState(false);
